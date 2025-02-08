4c2d12edf757038c8586f5ba44248d2e8b4efea7
JuanDCas-777 <juandicastrillon2206@gmail.com>
2025-02-08 15:49:53 +0000

    datos actualizados

---
+++ inicio.py
@@ -0,0 +1,11 @@
+edad = 22
+nombre = "maria"
+cover = 30000
+
+if edad < 18 : 
+    print ("no tiene permiso para ingresar")
+else :
+    print ("tienes la edad para ingresar")
+
+if cover < 40000:
+    print ("tu ere pobre no puede entra'")
