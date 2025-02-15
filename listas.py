0f48d1f470f8ab8f74e0489cf72eddac72a1348b
JuanDCas-777 <juandicastrillon2206@gmail.com>
2025-02-15 16:34:15 +0000

    lista

--- listas.py
+++ listas.py
@@ -1,3 +1,21 @@
 numeros=list()
-def agregar (numero:int):
-    numeros.append(numero)
+Continuar: bool = True
+
+def agregar (numero:int) -> None:
+  numeros.append(numero)
+
+def eliminar() -> None:
+  numeros.pop()
+
+while Continuar:
+  print ("seleccione una opcion")
+  print  ("1. agregar un numero")
+  print  ("2. eliminar un numero")
+  print  ("3. salir")
+  
+  opcion = int(input())
+
+  if opcion == 3:
+    Continuar = False
+  if opcion == 1: 
+   print ("ta bien")
