14ca2b6578df8ead3d02e0e959e94f33d6c36187
JuanDCas-777 <juandicastrillon2206@gmail.com>
2025-02-15 17:35:19 +0000

    diccionario

---
+++ diccionario.py
@@ -0,0 +1,27 @@
+persona=dict()
+continuar: bool=True
+ 
+
+def agregar_valor (clave:str,valor:str): 
+    persona.update({clave:valor})
+
+def eliminar() :
+    persona.pop(clave)
+
+while continuar: 
+ print("escoge un opcion")
+ print  ("1. agregar un valor")
+ print  ("2. eliminar un valor")
+ print  ("3. salir")
+
+ opcion = int(input())
+
+ if opcion == 3:
+     continuar=False
+ elif opcion == 1:
+        clave = input("Ingresa la clave: ")
+        valor = input("Ingresa el valor: ")
+        agregar_valor(clave, valor)
+        print(f"Diccionario actualizado: {persona}")
+ elif opcion == 2:
+     eliminar 
