9ca03003251b54f8a6c56814002e7ce82ce29520
JuanDCas-777 <juandicastrillon2206@gmail.com>
2025-02-22 17:40:25 +0000

    multiplicacion base suma

--- actividad recursion/3. multiplicacion con sumas (recursion).py
+++ actividad recursion/3. multiplicacion con sumas (recursion).py
@@ -1,7 +1,11 @@
-def multiplicar(a,b):  
+def multiplicar (a,b):
     if b==0:
-      return 
-    if b>0:
-      return a + multiplicar(a, b - 1 )
-    return multiplicar(a,-b)
-print(multiplicar(3, -4)) 
+      return 0
+    if b<0 :
+      return -multiplicar(a,-b)
+    return a + multiplicar (a,b-1)
+
+a=int(input("ingrese el primer numero: " ))
+b=int(input("ingrese el segundo numero: " ))
+resultado = multiplicar(a,b)
+print(f"la multiplicacion de a x b es: {resultado}")
