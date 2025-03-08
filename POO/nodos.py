42d8354d3d6dd5eba677d882156879ff42602567
JuanDCas-777 <juandicastrillon2206@gmail.com>
2025-03-08 15:19:58 +0000

    nodo

---
+++ POO/nodos.py
@@ -0,0 +1,28 @@
+from typing import Optional
+
+
+class Node:  
+    def __init__(self, numero:int)-> None: 
+        self.dato=numero
+        self.next:Optional["Node"]=None
+        
+        
+class listaenlazada:
+  def __init__(self) -> None:
+           self.cabeza:Optional["Node"]=None
+           
+  def agregar(self,numero:int)-> None:
+       nodo:Node=Node(numero)
+       if self.cabeza is None:
+         self.cabeza=nodo
+       else:
+         nodo_actual=self.cabeza
+         while nodo_actual.next is not None:
+            nodo_actual=nodo_actual.next
+         nodo_actual.next=nodo  
+
+lista=listaenlazada()
+lista.agregar(7)
+lista.agregar(23)
+lista.agregar(10)
+lista.agregar(6)        
