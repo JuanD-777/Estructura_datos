8faac69dc9066a10dd5f40e2a2323b51f92779fb
JuanDCas-777 <juandicastrillon2206@gmail.com>
2025-03-29 15:00:27 +0000

    practica colas resuelto

--- practicaColas/model/ticket.py
+++ practicaColas/model/ticket.py
@@ -8,6 +8,10 @@ class Ticket(BaseModel):
     age: int # age of the person
     priority_attention: bool = None # priority attention? True or False
 
+   #contructor with default value for priority_attention
+    # if age >= 60, priority_attention = True
+    # if age < 60, priority_attention = False
+    # if priority_attention is not provided, it will be set to True if age >= 60
     def __init__(self, **data):
         if "priority_attention" not in data or data["priority_attention"] is None:
             data["priority_attention"] = data["age"]>=60
