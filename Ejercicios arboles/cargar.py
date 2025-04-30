0437606a5b5ea6885a787191bf750761be2db7e2
JuanDCas-777 <juandicastrillon2206@gmail.com>
2025-04-30 22:01:39 +0000

    hospital 3 y 4

--- Ejercicios arboles/cargar.py
+++ Ejercicios arboles/cargar.py
@@ -17,6 +17,51 @@ class Nodo:
         self.derecha = None  
 
 
+
+class Arbolhospital:
+    def __init__(self):
+      self.raiz=None
+
+    def insertar(self, hospital):
+        if self.raiz is None:
+            self.raiz = Nodo (hospital)
+        else:
+            self._insertar_recursion(self.raiz, hospital)
+
+    def _insertar_recursion(self, nodo, hospital):
+        if hospital.nit < nodo.hospital.nit:
+            if nodo.izquierda is None:
+                nodo.izquierda = Nodo(hospital)
+            else:
+               self._insertar_recursion(nodo.izquierda, hospital)
+        else:
+            if nodo.derecha is None:
+                nodo.derecha = Nodo(hospital)
+            else:
+                self._insertar_recursion(nodo.derecha, hospital)
+
+
+    def inorden(self, nodo):
+        if nodo is not None:
+            self.inorden(nodo.izquierda)
+            print(nodo.hospital)
+            self.inorden(nodo.derecha)
+
+
+def buscar_hospital(arbol, nit_buscado): 
+    return _buscar_recursion(arbol.raiz, nit_buscado)
+
+def _buscar_recursion(nodo,nit_buscado):
+    if nodo is None:
+        return None
+    if nit_buscado == nodo.hospital.nit:
+        return nodo.hospital
+    elif nit_buscado < nodo.hospital.nit:
+        return _buscar_recursion(nodo.izquierda, nit_buscado)
+    else:
+        return _buscar_recursion(nodo.derecha, nit_buscado)
+
+
 hospitales = pd.read_csv('/workspaces/Estructura_datos/Ejercicios arboles/Directorio_E.S.E._Hospitales_de_Antioquia_con_coordenadas_20250426.csv')
 hospitales.rename(columns={
     'Razón Social Organización': 'nombre',
@@ -31,6 +76,7 @@ print(hospitales.dtypes)
 print(hospitales.columns)
 print(hospitales['nit'])
 
+arbol = Arbolhospital()
 
 for index, row in hospitales.iterrows():
     hospital = Hospital(
@@ -39,4 +85,19 @@ for index, row in hospitales.iterrows():
         sede=row['sede'],
         municipio=row['municipio']
     )
-    print(hospital)
+    arbol.insertar(hospital)
+
+print("Hospitales ordenados por NIT:")
+arbol.inorden(arbol.raiz)
+
+
+nit_a_buscar = int(input("Ingrese el NIT del hospital a buscar: "))
+resultado = buscar_hospital(arbol, nit_a_buscar)
+
+if resultado:
+    print("🏥 Hospital encontrado:")
+    print(f"Nombre de la organización: {resultado.nombre}")
+    print(f"Nombre de la sede: {resultado.sede}")
+    print(f"Municipio: {resultado.municipio}")
+else:
+    print("❌ Hospital no encontrado.")
